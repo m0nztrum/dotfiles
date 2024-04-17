f0bbec71a29136a2a65c613ebcc563574c50cc6a
delafiado@gmail.com <delafiado@gmail.com>
2024-04-17 22:42:04 +0000

    minor update

--- configs/hypr/scripts/screenshot.sh
+++ configs/hypr/scripts/screenshot.sh
@@ -1,45 +1,107 @@
-#!/usr/bin/env sh
-if [ -z "$XDG_PICTURES_DIR" ] ; then
-    XDG_PICTURES_DIR="$HOME/Pictures"
-fi
+#!/usr/bin/env bash
+# Borrowed from: https://github.com/JaKooLit  ##
+# Screenshots scripts
 
-ScrDir=`dirname "$(realpath "$0")"`
-swpy_dir="${XDG_CONFIG_HOME:-$HOME/.config}/swappy"
-save_dir="${2:-$XDG_PICTURES_DIR/Screenshots}"
-save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
-temp_screenshot="/tmp/screenshot.png"
+iDIR="$HOME/.config/swaync/icons"
+notify_cmd_shot="notify-send -h string:x-canonical-private-synchronous:shot-notify -u low -i ${iDIR}/picture.png"
 
-mkdir -p $save_dir
-mkdir -p $swpy_dir
-echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" > $swpy_dir/config
+time=$(date "+%y%m%d_%Hh%Mm%Ss")
+dir="$(xdg-user-dir)/Pictures/Screenshots"
+file="screenshot_${time}_${RANDOM}.png"
 
-function print_error
-{
-cat << "EOF"
-    ./screenshot.sh <action>
-    ...valid actions are...
-        p : print all screens
-        s : snip current screen
-        sf : snip current screen (frozen)
-        m : print focused monitor
-EOF
+active_window_class=$(hyprctl -j activewindow | jq -r '(.class)')
+active_window_file="screenshot_${time}_${active_window_class}.png"
+active_window_path="${dir}/${active_window_file}"
+
+# notify and view screenshot
+notify_view() {
+    if [[ "$1" == "active" ]]; then
+        if [[ -e "${active_window_path}" ]]; then
+            ${notify_cmd_shot} "Screenshot of '${active_window_class}' Saved."
+        else
+            ${notify_cmd_shot} "Screenshot of '${active_window_class}' not Saved"
+        fi
+    elif [[ "$1" == "swappy" ]]; then
+        ${notify_cmd_shot} "Screenshot Captured."
+    else
+        local check_file="$dir/$file"
+        if [[ -e "$check_file" ]]; then
+            ${notify_cmd_shot} "Screenshot Saved."
+        else
+            ${notify_cmd_shot} "Screenshot NOT Saved."
+        fi
+    fi
 }
 
-case $1 in
-p)  # print all outputs
-    grimblast copysave screen $temp_screenshot && swappy -f $temp_screenshot ;;
-s)  # drag to manually snip an area / click on a window to print it
-    grimblast copysave area $temp_screenshot && swappy -f $temp_screenshot ;;
-sf)  # frozen screen, drag to manually snip an area / click on a window to print it
-    grimblast --freeze copysave area $temp_screenshot && swappy -f $temp_screenshot ;;
-m)  # print focused monitor
-    grimblast copysave output $temp_screenshot && swappy -f $temp_screenshot ;;
-*)  # invalid option
-    print_error ;;
-esac
 
-rm "$temp_screenshot"
 
-if [ -f "$save_dir/$save_file" ] ; then
-  notify-send -a "Screenshot" -i "${save_dir}/${save_file}" -t 2200 "Screenshot saved at ${save_dir}/${save_file}"
+# countdown
+countdown() {
+    for sec in $(seq $1 -1 1); do
+        notify-send -h string:x-canonical-private-synchronous:shot-notify -t 1000 -i "$iDIR"/timer.png "Taking shot in : $sec"
+        sleep 1
+    done
+}
+
+# take shots
+shotnow() {
+    cd ${dir} && grim - | tee "$file" | wl-copy
+    sleep 2
+    notify_view
+}
+
+shotwin() {
+    w_pos=$(hyprctl activewindow | grep 'at:' | cut -d':' -f2 | tr -d ' ' | tail -n1)
+    w_size=$(hyprctl activewindow | grep 'size:' | cut -d':' -f2 | tr -d ' ' | tail -n1 | sed s/,/x/g)
+    cd ${dir} && grim -g "$w_pos $w_size" - | tee "$file" | wl-copy
+    notify_view
+}
+
+shotarea() {
+    tmpfile=$(mktemp)
+    grim -g "$(slurp)" - >"$tmpfile"
+    if [[ -s "$tmpfile" ]]; then
+        wl-copy <"$tmpfile"
+        mv "$tmpfile" "$dir/$file"
+    fi
+    rm "$tmpfile"
+    notify_view
+}
+
+shotactive() {
+    active_window_class=$(hyprctl -j activewindow | jq -r '(.class)')
+    active_window_file="screenshot_${time}_${active_window_class}.png"
+    active_window_path="${dir}/${active_window_file}"
+
+    hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' | grim -g - "${active_window_path}"
+    sleep 1
+    notify_view "active"
+}
+
+shotswappy() {
+    tmpfile=$(mktemp)
+    grim -g "$(slurp)" - >"$tmpfile" &&  notify_view "swappy"
+    swappy -f - <"$tmpfile"
+    rm "$tmpfile"
+}
+
+
+if [[ ! -d "$dir" ]]; then
+    mkdir -p "$dir"
 fi
+
+if [[ "$1" == "--now" ]]; then
+    shotnow
+elif [[ "$1" == "--win" ]]; then
+    shotwin
+elif [[ "$1" == "--area" ]]; then
+    shotarea
+elif [[ "$1" == "--active" ]]; then
+    shotactive
+elif [[ "$1" == "--swappy" ]]; then
+    shotswappy
+else
+    echo -e "Available Options : --now --win --area --active --swappy"
+fi
+
+exit 0
